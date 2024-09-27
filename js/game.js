c142b00af1fc8fb08716143e6706e5fef97fd641
Zaid Hassan <zaidhassan251@gmail.com>
2024-09-27 21:34:01 +0530

    fixed life limit

--- js/game.js
+++ js/game.js
@@ -86,7 +86,7 @@ export default class Game {
     }
     // Cleanup method
     checkGameOver() {
-        if (this.life <= 9) {
+        if (this.life <= 0) {
             
             return true;
         }
